d763f341900ddc2c7b99cd4058869980deeff3c9
Guillermo Reyes Gómez <guillebenya@uoc.edu>
2026-04-14 18:15:45 +0200

    Clase Role creada y EntityScan configurada para que detecte las entidades fuera del paquete de la clase principal (funciona)

--- backend/src/main/java/com/klubly/backend/KlublyApplication.java
+++ backend/src/main/java/com/klubly/backend/KlublyApplication.java
@@ -2,10 +2,14 @@ package com.klubly.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EntityScan(basePackages = {"com.klubly"})
+@EnableJpaRepositories(basePackages = {"com.klubly"})
 public class KlublyApplication {
 
 	public static void main(String[] args) {
